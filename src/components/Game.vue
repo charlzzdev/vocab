f5f3bd6f3cd37cb7bbdd9d34db59b1273d427b33
Charles <ellerkaresz@gmail.com>
2020-04-04 22:57:47 +0200

    add word list & add random word generation

---
+++ src/components/Game.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{randomWord}}</h1>
+    <button v-on:click="generateRandomWord">Generate random word</button>
+  </div>
+</template>
+
+<script>
+import wordList from '../assets/wordList';
+
+export default {
+  name: 'Game',
+  data: () => {
+    return {
+      randomWord: 'word'
+    };
+  },
+  methods: {
+    generateRandomWord: function() {
+      const { words } = wordList;
+      const randomWord = words[Math.random() * words.length | 0]; 
+      this.randomWord = randomWord;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
