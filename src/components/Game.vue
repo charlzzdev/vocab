adcec7e43cbc0e91bd7f694267ae68b023ba06bf
Charles <ellerkaresz@gmail.com>
2020-04-05 13:27:16 +0200

    add ability to guess

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>{{randomWord}}</h1>
-    <button v-on:click="generateRandomWord">Generate random word</button>
+    <h1>{{ currentWord }}</h1>
+    <div v-for="{ word, definition } in dictionary" v-bind:key="word">
+      <button v-on:click="guess">{{ definition }} ({{word}})</button>
+    </div>
   </div>
 </template>
 
@@ -12,15 +14,43 @@ export default {
   name: 'Game',
   data: () => {
     return {
-      randomWord: 'word'
+      currentWord: '',
+      dictionary: []
     };
   },
   methods: {
     generateRandomWord: function() {
       const { words } = wordList;
       const randomWord = words[Math.random() * words.length | 0]; 
-      this.randomWord = randomWord;
+      return randomWord;
+    },
+    generateDictionary: function(){
+      this.dictionary = [];
+      for(let i = 0; i < 3; i++){
+        this.dictionary = [
+          ...this.dictionary,
+          {
+            word: this.generateRandomWord(),
+            definition: `d${Math.random()*1000|0}`
+          }
+        ]
+      }
+
+      this.currentWord = this.dictionary[Math.random()*this.dictionary.length|0].word;
+    },
+    guess: function(e) {
+      const definition = e.target.innerText.split(" ")[0];
+      this.dictionary.forEach(d => {
+        if(d.word === this.currentWord && d.definition === definition){
+          console.log('correct');
+        }
+      });
+
+      this.generateDictionary();
     }
+  },
+  created: function () {
+    this.generateDictionary();
   }
 }
 </script>
