d540911f7a8c465fe7f693862c02c04018a080b1
Charles <ellerkaresz@gmail.com>
2020-04-19 11:40:41 +0200

    only ask words that have not been guessed 3 times

--- src/utils/generateRandomWord.js
+++ src/utils/generateRandomWord.js
@@ -1,9 +1,13 @@
 import wordList from '../assets/wordList';
+import store from '../store';
 
 function generateRandomWord(choices) {
   const { words } = wordList;
   const randomWord = words[Math.random() * words.length | 0];
-  let wordExists = false;
+  const userPointsByWord = store.state.user.data.points.byWord;
+  let wordExists = Object.entries(userPointsByWord).some(
+    ([word, points]) => word === randomWord && points >= 3
+  );
 
   choices.forEach(choice => {
     if(choice.word === randomWord) wordExists = true;
